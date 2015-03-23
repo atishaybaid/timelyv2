e42750de532ca24cd7b39b85a41ed0f3339322f9
atishaybaid <atishaybaid@gmail.com>
2015-03-23 10:55:21 -0800

    local storage initilazed

--- app/src/scripts/services/toDoService.js
+++ app/src/scripts/services/toDoService.js
@@ -1,23 +1,18 @@
-TimelyApp.service("toDoService",function(){
- 
- var newTask ={};
- var taskList = [];
-
-this.addTask = function(newTask){
-	console.log("inside add task");
-
-	
-	
-	if(event.keyCode === 13){
-		console.log(newTask);	
-
-
-	}
-
-
-};
-
-
-
-
+TimelyApp.service("toDoService", function() {
+    var newTask = {};
+    var taskList = [];
+    var Task = function(newTaskName) {
+        this.taskName = newTaskName;
+        this.isCompleated = 'false';
+    };
+    this.addTask = function(newTaskName) {
+        if (event.keyCode === 13) {
+            var newTask = new Task(newTaskName);
+            this.storeToLocalStorage(newTask);
+        };
+    };
+    this.storeToLocalStorage = function(newTask) {
+        var key = 'newTask';
+        window.localStorage.setItem(key, angular.toJson(newTask));
+    };
 });
