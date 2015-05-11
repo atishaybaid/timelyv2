a4ee2dbb3cd70d3e26b535b2bc8fc04ca2091462
atishaybaid <atishaybaid@gmail.com>
2015-05-11 10:08:25 -0800

    youTube  player working

--- app/src/scripts/directives/youTubeDirective.js
+++ app/src/scripts/directives/youTubeDirective.js
@@ -1,40 +1,25 @@
 TimelyApp.directive('youtube', function($window) {
     var directive = {};
     var player;
-    
     directive.restrict = 'E';
-
-    directive.template = '<div id="myPlayer"></div>';
-    
-    directive.compile = function(element, attribute) {
-    	console.log("compile working");
-        var scriptTag = document.createElement("script");
-        scriptTag.src = "http://www.youtube.com/iframe_api";
-        var orignalScriptTag = document.getElementsByTagName('script')[0];
-        console.log(orignalScriptTag.parentNode);
-        orignalScriptTag.parentNode.insertBefore(scriptTag, orignalScriptTag);
-        var link = function($scope, element, attribute) {
-            $window.onYouTubeIframeAPIReady = function() {
-                player = new YT.Player('myPlayer', {
-                    height: '390',
-                    width: '670',
-                    events: {
-                        'onReady': onPlayerReady,
-                    }
-                });
-            };
+    directive.template = '<div id="myPlayer" style="visibility:hidden"></div>';
+    directive.link = function($scope, element, attribute) {
+        $window.onYouTubeIframeAPIReady = function() {
+            player = new YT.Player('myPlayer', {
+                height: 0,
+                width: 0,
+                videoId:'OG0xt2xTq4A',
+                events: {
+                    'onReady': onPlayerReady,
+                }
+            });
         };
-
-        return link;
     };
+    $window.onPlayerReady = function(event) {
+        console.log("at onReady");
+        event.target.loadPlaylist("_SaEl4cKaAo,w_HaezV0DqI,I4d0KU-SuBc");
 
-    $window.onPlayerReady = function(event){
-    	console.log(event);
-                        	//player.cuePlaylist(["OG0xt2xTq4A","jOYR3k1VhUQ"]);
-                        //event.target.playVideo();
-                        	//player.playVideo();
-
+        player.playVideo();
     };
-
     return directive;
-})
+});
